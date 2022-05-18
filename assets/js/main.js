eb408e69d54c229f338a67e7c865775f5624f095
Kadi <gueneykadircan@gmail.com>
2022-05-18 15:15:55 +0200

    ready

---
+++ assets/js/main.js
@@ -0,0 +1,226 @@
+// console.log("text");
+
+// let input = document.getElementById('inputEmail');
+// let emailArr = ["batman@batcave.gotham", "flash@superspeed.nyc", "C3PO@droids.com", "R2D2@droid.com"]
+
+// // let check = () => {
+// //     if (input.value == emailArr[0] || input.value == emailArr[1] || input.value == emailArr[2] || input.value == emailArr[3]) {
+// //         console.log("vergeben");
+// //     } else if 
+// //         console.log("nicht vergeben");
+// // }
+
+// let check = () => {
+//     if (emailArr.indexOf(input.value) >= 0) {
+//         console.log("vergeben");
+//     } else {
+//         console.log("nicht vergeben")
+//     }
+// }
+
+// ==================================================
+// Lev 1-1 
+// ==================================================
+
+let adress = ["Mittwegstraße 17", "45127 Essen", "Nordrhein-Westfahlen"];
+// console.log(adress);
+
+let freunde = ["Julia", "Charly", "Julian"];
+// console.log(freunde);
+
+let person = ["Kadircan", "Güney", "Kadi", "26", "Musik, Schwimmen, Coden", "Moers", "SuperCode"];
+// console.log(person);
+
+// console.log(adress, freunde, person)
+
+// person.push(adress, freunde);
+// console.log(person);
+
+// ==================================================
+// Lev 1-1-2
+// ==================================================
+
+// console.log(person.length) // 7
+// console.log(adress.length) // 3
+// console.log(freunde.length) // 3
+
+// =================================================
+// Lev 1-2
+// ==================================================
+
+// let meineTrainer1 = ["Eric", "Steffen", "Simon", "Ahmed"]
+// let meineTrainer2 = new Array("Max", "Steffen", "Simon", "Ahmed");
+
+// console.log(meineTrainer1);
+// console.log(meineTrainer2);
+
+// let meineTrainer3 = new Array();
+// meineTrainer3[0] = "Eric";
+// meineTrainer3[1] = "Steffen";
+// meineTrainer3[2] = "Simon";
+// meineTrainer3[3] = "Ahmed";
+
+// console.log(meineTrainer3);
+
+// =================================================
+// Lev 1-3
+// ==================================================
+
+// let numberArray = [5, 6, 7, 8, 9, 10];
+// console.log(numberArray);
+
+// console.log(numberArray[4]);
+// console.log(numberArray[0]);
+// console.log(numberArray[5]);
+
+// =================================================
+// Lev 1-4
+// ==================================================
+
+// let meinText1 = "Hallo Welt. Ich wünsche euch allen einen guten Arbeitstag."
+// let meinText2 = "Wie geht es dir heute?"
+// let meinText3 = "Heute ist ein großer Tag für uns."
+
+// let split1 = meinText1.split();
+// let split2 = meinText1.split("");
+// let split3 = meinText1.split(" ");
+
+// console.log(split1);
+// console.log(split2);
+// console.log(split3);
+
+// let split4 = meinText2.split();
+// let split5 = meinText2.split("");
+// let split6 = meinText2.split(" ");
+
+// console.log(split4);
+// console.log(split5);
+// console.log(split6);
+
+// let split7 = meinText3.split();
+// let split8 = meinText3.split("");
+// let split9 = meinText3.split(" ");
+
+// console.log(split7);
+// console.log(split8);
+// console.log(split9);
+
+// =================================================
+// Lev 1-5-1
+// ==================================================
+
+// let songs = ["Bohemian Rhapsody", "Stairway to Heaven", "Hotel California"];
+
+// let totalSongs = songs.push("Mystery of Love", "Experience", "Only the Young");
+
+// console.log(totalSongs);
+// console.log(songs);
+
+// let fusballer = ["Daddy Macron", "Merkel", "Putin", "Donald", "Erdogan"];
+// let torhut = fusballer.push("fußball", "ist", "primitiv");
+// console.log(fusballer)
+
+
+// =================================================
+// Lev 1-5-2
+// ==================================================
+
+// let heroUndEnemy = [["Batman", "The Joker"], ["Professor X", "Magneto"], ["Thor", "Loki"]];
+// heroUndEnemy.push[["Spiderman", "The Avatar"], ["Sokka", "Katara"], ["Zuko", "Azula"]];
+// console.log(heroUndEnemy);
+
+// =================================================
+// Lev 1-6
+// ==================================================
+
+// let songs = ["Bohemian Rhapsody", "Stairway to Heaven", "Hotel California"];
+// let totalSongs = songs.push("Mystery of Love", "Experience", "Only the Young");
+
+// console.log(songs);
+// console.log(totalSongs);
+
+// let entfernterSong = songs.pop();
+// console.log(entfernterSong);
+
+
+// let fusballer = ["Daddy Macron", "Merkel", "Putin", "Donald", "Erdogan"];
+// let torhut = fusballer.push("fußball", "ist", "primitiv");
+
+// let entfernterFussballer = fusballer.pop();
+// console.log(entfernterFussballer);
+
+// =================================================
+// Lev 1-7
+// ==================================================
+
+// let deutscheGerichte = ["Speckkuchen", "Thürunger Rostbratwurst", "Quarkkeulchen", "Sauerbraten"];
+// console.log(deutscheGerichte);
+
+// deutscheGerichte.unshift("Döner", "Kartoffelauflau", "Schwarzbrot", "Pommes", "Brliner")
+// console.log(deutscheGerichte);
+
+// =================================================
+// Lev 1-8
+// ==================================================
+
+// let deutscheGerichte = ["Speckkuchen", "Thürunger Rostbratwurst", "Quarkkeulchen", "Sauerbraten"];
+// console.log(deutscheGerichte);
+
+// let nichtGut = [deutscheGerichte.shift(), deutscheGerichte.shift(), deutscheGerichte.shift()];
+// console.log(deutscheGerichte);
+
+// =================================================
+// Lev 1-9
+// ==================================================
+
+// let array = [23, 54, 75];
+// console.log(array);
+
+// array.push(5, 10, 15, 20, 25);
+// console.log(array);
+
+// array.unshift(6, 12, 18, 24, 30);
+// console.log(array);
+
+// array.pop();
+// array.pop();
+// console.log(array);
+
+// array.shift();
+// array.shift();
+// console.log(array);
+
+// =================================================
+// Lev 1-12
+// ==================================================
+
+let array = [
+    "imageTeilnehmer000supercode.jpg",
+    "imageTeilnehmer001supercode.jpg",
+    "imageTeilnehmer002supercode.jpg",
+    "imageTeilnehmer003supercode.jpg",
+    "imageTeilnehmer004supercode.jpg",
+    "imageTeilnehmer005supercode.jpg",
+    "imageTeilnehmer006supercode.jpg",
+    "imageTeilnehmer007supercode.jpg",
+    "imageTeilnehmer008supercode.jpg",
+    "imageTeilnehmer009supercode.jpg",
+    "imageTeilnehmer010supercode.jpg",
+    "imageTeilnehmer011supercode.jpg",
+    "imageTeilnehmer012supercode.jpg",
+    "imageTeilnehmer013supercode.jpg",
+    "imageTeilnehmer014supercode.jpg",
+    "imageTeilnehmer015supercode.jpg",
+    "imageTeilnehmer016supercode.jpg",
+    "imageTeilnehmer017supercode.jpg",
+    "imageTeilnehmer018supercode.jpg",
+    "imageTeilnehmer019supercode.jpg"
+];
+
+let copyImg1 = array.slice(007, 014);
+console.log(array);
+console.log(copyImg1);
+
+let copyImg2 = array.slice(006, 011);
+console.log(copyImg2);
+
